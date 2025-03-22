e13b4639babed24d8fe407af15c8d23c26880d5b
GARAPATI HARSHITA <garapatiharshita123@gmail.com>
2025-03-22 12:03:53 +0530

    problem2.py

---
+++ problem2.py
@@ -0,0 +1,25 @@
+#In this program,we have to print table by using REPL means terminal
+#go to the terminal and then use 'python' to start coding in it
+#Then simply give the table and print it....
+#Below given is the visual code in REPL
+5*1
+5
+5*2
+10
+5*3
+15
+5*4
+20
+5*5
+25
+5*6
+30
+5*7
+35
+5*8
+40
+5*9
+45
+5*10
+50
+
