6ff66aaa844cf4c8f135b27bb836466487e03b94
GARAPATI HARSHITA <garapatiharshita123@gmail.com>
2025-03-22 12:04:32 +0530

    Create problem3.py

---
+++ problem3.py
@@ -0,0 +1,7 @@
+#Program to use external modules pyttsx3 is an external module in which we can hear the print statement
+#External module means some one built the code and simply we are going to use for your purpose
+#For this module goto crome and search for pyttsx3 then simply copy code in that and paste it in the platform which is using by you and run it
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey Devi sree vara prasad!What are you Doing")
+engine.runAndWait()
